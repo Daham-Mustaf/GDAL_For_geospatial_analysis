23a908d0d092fc1d403481d22107538a89b1402d
Daham-Mustaf <s7damust@uni-bonn.de>
2021-09-09 22:36:40 +0200

    Report about row reocrds

--- myscript.sh
+++ myscript.sh
@@ -9,6 +9,8 @@ OgrInfo -so countries.shp -sql " SELECT * FROM countries"
 
 ogr2ogr countries.shp -t_srs "EPSG:4326" /Users/m-store/Desktop/GDAL/new new_count.shp
 ogr2ogr/Users/m-store/Desktop/GDAL/new countries.shp -t_srs "EPSG:4326" /Users/m-store/Desktop/GDAL/new_count.shp
+ogr2ogr -sql "SELECT * FROM countries WHERE NAME='Germany'" germany.shp countries.shp
+
 OgrInfo --formats countries.shp 
 
 ogr2ogr -f GeoJSON -t_srs "EPSG:4326" country.geojson countries.shp
