878ddf912d16752c153985a1923f5490d2e0a85f
Daham-Mustaf <s7damust@uni-bonn.de>
2021-09-09 21:00:44 +0200

    Updated bash

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,12 @@
 OgrInfo countries.shp
 
+OgrInfo -ro -so countries.shp
 OgrInfo -so countries.shp
 OgrInfo -so countries.shp -sql " SELECT * FROM countries"
+
+/Users/m-store/Desktop/GDAL/new
+
+ogr2ogr countries.shp -t_srs "EPSG:4326" /Users/m-store/Desktop/GDAL/new new_count.shp
+ogr2ogr/Users/m-store/Desktop/GDAL/new countries.shp -t_srs "EPSG:4326" /Users/m-store/Desktop/GDAL/new_count.shp
+
+$ OgrInfo --formats countries.shp 
